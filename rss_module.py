9498950ea0c28d19f28df8696bebe3d65acb9d28
mattdeboard <matt.deboard@gmail.com>
2010-12-24 21:18:51 -0500

    another todo

--- rss_module.py
+++ rss_module.py
@@ -1,5 +1,5 @@
 import sys
-import feedparser
+import feedparser, BeautifulSoup
 from calendar import timegm
 from time import gmtime
 
@@ -7,26 +7,29 @@ from time import gmtime
 # 1. More authoritative/less hacky method for determining authenticity of RSS url
 # 2. Regex matching for more reliable determination of what key,value pair from the feed to use as the timestamp
 #    field
-#
+# 3. Add link-mining capability.
 
-def rssdownload(feedurl, last_reference):
-	messages = []
-	feed = feedparser.parse(feedurl)
-	try: feed.feed.title
-	except AttributeError: return 'The URL provided does not appear to be a valid RSS feed.'
-        for item in feed.entries:
-            if timegm(item.updated_parsed) > last_reference:
-                messages.append({'url':item.link,
-                                 'timestamp':item.updated_parsed,
-                                 'description':item.title,
-                                 'extra':feed.feed.link,
-                                 'refer':''})
-        if len(messages) == 0:
-            return 'There do not seem to be any new links since the last update.'
-	last_ref = timegm(messages[len(messages)-1]['timestamp'])
-	return{'messages':messages,
-               'last_reference':last_ref,
-               'protected':False}
+def rssdownload(feedurl, last_reference=0):
+    messages = []
+    feed = feedparser.parse(feedurl)
+    try: feed.feed.title
+    except AttributeError: return 'The URL provided does not appear to be a valid RSS feed.'
+    for item in feed.entries:
+        if timegm(item.updated_parsed) > last_reference:
+            messages.append({'url':item.link,
+                             'timestamp':item.updated_parsed,
+                             'description':item.title,
+                             'extra':feed.feed.link,
+                             'refer':''})
+    if len(messages) == 0:
+        return 'There do not seem to be any new links since the last update.'
+    last_ref = timegm(messages[len(messages)-1]['timestamp'])
+    return{'messages':messages,
+           'last_reference':last_ref,
+           'protected':False}
+
+
+    
 	
 if __name__ == "__main__":
     sample_timestamp = timegm(gmtime()) - 15000 #41 minutes prior to start of the test, simply for testing purposes
