81183d6535380e878212218f25eb40b12de47030
Matt DeBoard <matt.deboard@gmail.com>
2010-12-30 09:31:35 -0500

    cleaning up output

--- rss_module.py
+++ rss_module.py
@@ -5,20 +5,6 @@ from time import gmtime
 from operator import itemgetter
 from lxml import html
 
-##Copyright (c) 2010, 2011 Matt DeBoard - mattdeboard.net - matt.deboard@gmail.com
-##
-##Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
-##(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
-##publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
-##so, subject to the following conditions:
-##
-##The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-##
-##THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-##MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-##FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-##WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
@@ -115,9 +101,7 @@ def rssdownload(username, feedurl, last_reference=0, mode=0):
                  'last_reference':last_ref,
                  'protected':False}
 
-    if len(deeplinks):
-        return feed_data, deeplinks
-    else: return feed_data
+    return feed_data, deeplinks
 
 def linkmine(summary):
     return [item[2] for item in html.iterlinks(summary)]
